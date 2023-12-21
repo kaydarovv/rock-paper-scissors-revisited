c8b8aac9ff8d42fa00065505460716041269afff
kaidarov <sanzharkaidarovv@gmail.com>
2023-12-22 01:57:02 +0600

    Add the full game logic

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
+let user = "none";
+let score = document.querySelector("#score");
+let menu = document.querySelector('#menu');
+let userScore = 0;
+let compScore = 0;
+let round = 0;
+let winner = 'noone';
+
+// RESET BUTTON
+let start = document.querySelector("#start-btn");
+
+start.addEventListener('click', () => {
+    userScore = 0;
+    compScore = 0;
+    winner = 'noone';
+    round = 0;
+});
+
 function compChoice(){
     return Math.floor(Math.random() * (3-1+1)) + 1;
 };
@@ -12,7 +30,7 @@ function playRound(userChoice){
 
     if (user == comp){
         winner = 'Noone';
-    } else if (((comp == 'rock')) && ((user == 'scissors')) || (comp == 'paper') && (user == 'rock') || (comp == 'scissors') && (user == 'paper')){
+    } else if (((comp === 'rock')) && ((user === 'scissors')) || (comp === 'paper') && (user === 'rock') || (comp === 'scissors') && (user === 'paper')){
         winner = 'Computer';
     } else {
         winner = 'User';
@@ -21,55 +39,25 @@ function playRound(userChoice){
     return [user, comp, winner];
 };
 
-function displayScore(div, results){
-    div.textContent = `Your choice: ${results[0]} \n Computer's choice: ${results[1]} \n And the winner is ${results[2]}`;
-}
+function displayScore(div, results, userScore, compScore, round){
+    div.textContent = `Round - ${round}. Your choice: ${results[0]}. Computer's choice: ${results[1]}. And the current score is: User - ${userScore}, Computer - ${compScore}`;
+    if (round === 5){
+        winner = (userScore > compScore) ? "User" 
+            : (userScore < compScore) ? "Computer" 
+            : "Noone";
+        div.textContent += `. The Final winner is: ${winner}`;
+    };
+};
 
-let user = "none";
-let score = document.querySelector("#score");
-let menu = document.querySelector('#menu');
 menu.addEventListener("click", (event) => {
     let target = event.target;
     user = target.id;
     let results = playRound(user);
-    console.log(results);
-    displayScore(score, results)
+    if (results[2] == 'Computer') {
+        compScore += 1;
+    } else if (results[2] == 'User') {
+        userScore += 1;
+    };
+    round += 1
+    displayScore(score, results, userScore, compScore, round)
 });
-
-
-// GAME LOOP FOR 5 ROUNDS MATCH
-
-// function game(){
-//     let winner = "none";
-//     let user_score = 0;
-//     let comp_score = 0;
-
-//     console.log("Hi! Let's play rock, paper, scissors! The rules are simple, 5 rounds of you vs computer. Input either r, p, s to choose your shot.");
-
-//     for (let i = 0; i<5; i++){
-//         winner = playRound();
-//         if (winner == "comp"){
-//             comp_score++;
-//             console.log("Oops, this round goes to the computer")
-//             console.log(`The score is: \n Computer: ${comp_score} \n User: ${user_score}`)
-//         } else if (winner == "user"){
-//             user_score++;
-//             console.log("Hooray, this round goes to the user")
-//             console.log(`The score is: \n Computer: ${comp_score} \n User: ${user_score}`)
-//         } else {
-//             console.log("A Draw!");
-//             console.log(`The score is: \n Computer: ${comp_score} \n User: ${user_score}`)
-//         }
-//     }
-
-//     if (comp_score > user_score){
-//         console.log("Computer wins!");
-//     } else if (user_score > comp_score) {
-//         console.log("User wins");
-//     } else {
-//         console.log("A Draw");
-//     }
-//     console.log(`The final score is: \n Computer: ${comp_score} \n User: ${user_score}`)
-// }
-
-//game();
